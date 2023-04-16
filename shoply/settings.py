c2bc494c0c50371274e99610f8d84a5f66816435
Vikas <vikas1618072@gmail.com>
2023-04-16 14:15:24 +0530

    fix(add os): add os

--- shoply/settings.py
+++ shoply/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 from django.utils.crypto import get_random_string
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
